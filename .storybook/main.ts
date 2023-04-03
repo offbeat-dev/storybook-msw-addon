8f4029723f32a344f0c2db274c46658f552654bc
offbeat-dev <offbeat.it.all@gmail.com>
2023-04-03 15:15:50 -0500

    :bug: -- fix conflicting issue with other decorators

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "storybook-addon-grid-overlay",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
